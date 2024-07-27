7d7975467f08f71053e4fd6154db956dcb7f86b7
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-07-27 15:56:01 -0300

    feat: adicionado rotas de carrinho, order, e historico em APp.tsc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import CategoryPage from "./app/admin/pages/categoryPage";
 import UserPage from "./app/admin/pages/userPage";
 import MenuPage from "./app/admin/pages/menuPage";
 import AdminPage from "./app/admin/pages/adminPage";
-import StatsPage from "./app/admin/pages/statsPage";
+//import StatsPage from "./app/admin/pages/statsPage";
 import RegisterPage from "./app/login/pages/registerPage";
 import LoginPage from "./app/login/pages/loginPage";
 import OrderPage from "./app/order/pages/orderPage";
@@ -48,10 +48,6 @@ const router = createBrowserRouter([
       {
         path: 'categorias',
         Component: CategoryPage
-      },
-      {
-        path: 'estatisticas',
-        Component: StatsPage
       }
     ]
   }
