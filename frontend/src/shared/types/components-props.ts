da06f23fd09d5e10ae27891b29ca3f4492ec5ec8
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-29 12:47:35 -0300

    chore: Remove unused CSS styles from ListOrder component

--- frontend/src/shared/types/components-props.ts
+++ frontend/src/shared/types/components-props.ts
@@ -1,13 +1,5 @@
 import { SvgIconComponent } from "@mui/icons-material";
 
-export interface ListOrderProps {
-  name: string;
-  totalPrice: string;
-  timeToDelivery: string;
-  items: string[];
-  editButtonCallback?: () => void;
-  editDisabled?: boolean;
-}
 export interface BaseLayoutListItemProps {
   text: string;
   icon: SvgIconComponent;
