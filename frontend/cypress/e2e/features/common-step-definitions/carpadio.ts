1a8ee02be9f89331379f99b25f98ebf49674a498
Luís Paulo Silva Trevisan <lpst@cin.ufpe.br>
2024-07-29 19:52:21 -0300

    add test: menu test

--- frontend/cypress/e2e/features/common-step-definitions/carpadio.ts
+++ frontend/cypress/e2e/features/common-step-definitions/carpadio.ts
@@ -1,6 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('Eu estou na pagina {string}', (page: string) => {
+Given('Eu estou na pagina: {string}', (page: string) => {
     cy.visit(page);
 });
 
@@ -9,5 +9,17 @@ When('Eu vou para a pagina {string}', (page: string) => {
 });
 
 Then('Eu estou na pagina', () => {
-    
+    cy.url().should('include', '/cardapio');
+});
+
+Given('Eu estou na pagina {string}', (page: string) => {
+    cy.visit(page);
+});
+
+When('Eu clico no botao criar', () => {
+    cy.get('[data-cy="criar-item"]').click();
+});
+
+Then('Eu cliquei no botao', () => {
+
 });
--- frontend/cypress/e2e/features/tests/cardapio.feature
+++ frontend/cypress/e2e/features/tests/cardapio.feature
@@ -1,6 +1,11 @@
 Feature: Cardapio
 
   Scenario: Ir ao cardapio
-    Given Eu estou na pagina "/"
+    Given Eu estou na pagina: "/"
     When Eu vou para a pagina "/adm/cardapio"
     Then Eu estou na pagina
+
+  Scenario: Criar itens
+    Given Eu estou na pagina "/adm/cardapio"
+    When Eu clico no botao criar
+    Then Eu cliquei no botao
